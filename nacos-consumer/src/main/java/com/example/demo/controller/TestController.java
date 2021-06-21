76e0cf4ebeb7dbe7d8175a02a584598604b04680
huangyt3 <huangyt3@asiainfo.com>
2021-06-21 09:37:54 +0800

    学习nacos的使用

---
+++ nacos-consumer/src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * created by huangyating
+ *
+ * @Date 2021/6/19
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public TestController(RestTemplate restTemplate) {this.restTemplate = restTemplate;}
+
+    @RequestMapping(value = "/echo/{str}", method = RequestMethod.GET)
+    public String echo(@PathVariable String str) {
+        return restTemplate.getForObject("http://provider/echo/" + str, String.class);
+    }
+}
